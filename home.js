dd9d5ed17ef936d762014ae3fe3844d54022a4e6
riamlawal <105566543+riamlawal@users.noreply.github.com>
2023-02-24 16:34:38 +0000

    Add files via upload

---
+++ home.js
@@ -0,0 +1,38 @@
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  ceneteredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+var swiper = new Swiper(".mytestimonial", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+   },autoplay:{
+    delay:2500,
+    disableOnInteraction:false,
+   }
+  
+});
+
+
+
+
+
+
+
